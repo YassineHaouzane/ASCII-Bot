6dc0b72c9764d89fc6a899437c6ad9f85ed66bd2
Yassine Haouzane <haouzaneyassine@gmail.com>
2022-02-23 01:49:29 +0100

    No need for local config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
-const  { clientId, guildId, token } = require('./config_local');
-
 module.exports = {
-    "clientId": process.env.CLIENT_ID || clientId ,
-	"guildId": process.env.GUILD_ID || guildId,
-    "token": process.env.DISCORD_TOKEN || token 
+    "clientId": process.env.CLIENT_ID,
+	"guildId": process.env.GUILD_ID,
+    "token": process.env.DISCORD_TOKEN 
 }
 
